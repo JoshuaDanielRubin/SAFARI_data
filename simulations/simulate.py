32c83be72bcee996892453fb8da0c1e3bb5afa9f
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-06-15 17:33:31 +0200

    Update

--- simulations/simulate.py
+++ simulations/simulate.py
@@ -33,13 +33,15 @@ def generate_reads(S, N, L, delta):
     for _ in range(N):
         start = random.randint(0, len(S) - L)
         read = list(S[start:start+L])
+        deaminated_bases = set()
         for i in range(len(read)):
-            if random.random() < delta:
+            if (read[i] == 'C' or read[i] == 'G') and random.random() < delta:
                 if read[i] == 'C':
                     read[i] = 'T'
                 elif read[i] == 'G':
                     read[i] = 'A'
-        reads.append((''.join(read), start))
+                deaminated_bases.add(i)
+        reads.append((''.join(read), start, deaminated_bases))
     return reads
 
 def find_seeds(index, read, k, origin):
@@ -67,6 +69,40 @@ def calculate_injectivity(rymer_minimizer_map):
     injectivity = total_rymers / len(unique_minimizers)
     return injectivity
 
+def compute_rymer_spuriousness(minimizer_seeds, rymer_seeds):
+    minimizer_kmers = {pos for pos, _ in minimizer_seeds}
+    rymer_kmers = {pos for pos, correct in rymer_seeds if correct}
+
+    # Kmers where the minimizer does not match but the rymer does, and is correct
+    spurious_rymer_kmers = rymer_kmers.difference(minimizer_kmers)
+
+    # Fraction of these kmers over the total number of rymer seeds
+    if rymer_kmers:
+        rymer_spuriousness = 1 - (len(spurious_rymer_kmers) / len(rymer_kmers))
+    else:
+        rymer_spuriousness = 0
+
+    return rymer_spuriousness
+
+def compute_rymer_recovery(minimizer_seeds, rymer_seeds, deaminated_bases, k):
+    minimizer_kmers = {pos for pos, _ in minimizer_seeds}
+    rymer_kmers = {pos for pos, correct in rymer_seeds if correct}
+
+    # Kmers where the minimizer does not match but the rymer does, and is correct
+    spurious_rymer_kmers = rymer_kmers.difference(minimizer_kmers)
+
+    # Of these, the ones that can be explained by deamination
+    deaminated_kmers = {pos+i for pos in spurious_rymer_kmers for i in range(k)}  # Flattened
+    deaminated_rymer_kmers = deaminated_kmers.intersection(deaminated_bases)
+
+    # Fraction of these kmers over the total number of spurious rymer kmers
+    if spurious_rymer_kmers:
+        rymer_recovery_rate = len(deaminated_rymer_kmers) / len(deaminated_kmers)
+    else:
+        rymer_recovery_rate = 0
+
+    return rymer_recovery_rate
+
 def main(N, L, delta, k, W):
     S = generate_dna(10000)
     minimizer_index, rymer_index, rymer_minimizer_map = create_minimizer_index(S, W, k)
@@ -76,8 +112,10 @@ def main(N, L, delta, k, W):
     total_rymer_seeds = 0
     minimizer_precision = 0
     rymer_precision = 0
+    rymer_spuriousness_sum = 0
+    rymer_recovery_sum = 0
 
-    for read, origin in reads:
+    for read, origin, deaminated_bases in reads:
         minimizer_seeds = find_seeds(minimizer_index, read, k, origin)
         rymer_read = ''.join(['R' if base in 'GA' else 'Y' for base in read])
         rymer_seeds = find_seeds(rymer_index, rymer_read, k, origin)
@@ -89,11 +127,17 @@ def main(N, L, delta, k, W):
         total_rymer_seeds += rymer_seeds_found
         minimizer_precision += minimizer_seed_precision
         rymer_precision += rymer_seed_precision
+        rymer_spuriousness = compute_rymer_spuriousness(minimizer_seeds, rymer_seeds)
+        rymer_spuriousness_sum += rymer_spuriousness
+        rymer_recovery_rate = compute_rymer_recovery(minimizer_seeds, rymer_seeds, deaminated_bases, k)
+        rymer_recovery_sum += rymer_recovery_rate
 
     print(f'Total minimizer seeds: {total_minimizer_seeds}')
     print(f'Total rymer seeds: {total_rymer_seeds}')
     print(f'Minimizer precision: {minimizer_precision / len(reads) if len(reads) > 0 else 0}')
     print(f'Rymer precision: {rymer_precision / len(reads) if len(reads) > 0 else 0}')
+    print(f'Rymer spuriousness: {rymer_spuriousness_sum / len(reads) if len(reads) > 0 else 0}')
+    print(f'Rymer recovery rate: {rymer_recovery_sum / len(reads) if len(reads) > 0 else 0}')
 
     injectivity = calculate_injectivity(rymer_minimizer_map)
     print(f'Injectivity: {injectivity}')
