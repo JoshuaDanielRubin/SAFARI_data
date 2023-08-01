f39ce44f3d5e9608347a75780247aefd569a467e
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-08-01 19:00:21 +0200

    Update

--- vg_corrected/src/minimizer_mapper.cpp
+++ vg_corrected/src/minimizer_mapper.cpp
@@ -642,7 +642,7 @@ for (const auto& kv : kmer_freq_map) {
 auto apply_rymer_filter = [&](const vector<Seed>& seeds_rymer,
                                const std::multimap<std::pair<size_t, pos_t>, Seed>& rymer_to_minimizer,
                                std::unordered_map<std::string, int> kmer_freq_map,
-                               int total_minimizers) {
+                               int total_minimizers, auto &minimizers, auto &rymers) {
 
     vector<Seed> filtered_seeds;
     size_t initial_rymers = seeds_rymer.size();
@@ -667,14 +667,14 @@ auto apply_rymer_filter = [&](const vector<Seed>& seeds_rymer,
 
         std::unordered_set<string> unique_minimizers;
         for (auto it = its.first; it != its.second; ++it) {
-            unique_minimizers.insert(it->second.seq);
+            unique_minimizers.insert(minimizers[seed.source].value.key.decode(minimizers[seed.source].length));
         }
 
-        string seq = "ACACA";
+        string seq = minimizers[seed.source].value.key.decode(minimizers[seed.source].length);
 
         cerr << "Unique minimizer sequences for Rymer sequence " << seq << ":\n";
         for (const auto& minimizer_seq : unique_minimizers) {
-            //cerr << "\t" << minimizer_seq << "\n";
+            cerr << "\t" << minimizer_seq << "\n";
 
             int raw_count = kmer_freq_map[minimizer_seq];
             double minimizer_freq = static_cast<double>(raw_count) / total_minimizers;
@@ -704,7 +704,7 @@ auto apply_rymer_filter = [&](const vector<Seed>& seeds_rymer,
 };
 
 // Use the lambda function
-//seeds_rymer = apply_rymer_filter(seeds_rymer, rymer_to_minimizer, kmer_freq_map, total_minimizers);
+seeds_rymer = apply_rymer_filter(seeds_rymer, rymer_to_minimizer, kmer_freq_map, total_minimizers, minimizers, minimizers_rymer);
 
 //if (seeds_rymer.empty()){throw runtime_error("[VG Giraffe] No RYmers passed filtering");}
 
