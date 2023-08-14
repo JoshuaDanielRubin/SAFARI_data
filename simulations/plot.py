da4bf9ac1e1c2711731029bc409e035f11971a83
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-08-14 15:51:21 +0200

    Update plots

--- simulations/plot.py
+++ simulations/plot.py
@@ -6,46 +6,29 @@ import os
 colors_minimizer = ['#A6CEE3', '#1F78B4']  # Light Blue, Dark Blue
 colors_rymer = ['#FB9A99', '#E31A1C']  # Light Red, Dark Red
 
-def generate_single_plot_save(k):
-    df_k = df[df['k'] == k]
-    df_grouped = df_k.groupby(['N', 'Minimizer sketch']).mean()[['Total minimizer seeds', 'Total rymer seeds']]
+# Function to generate stratified plots by a given column
+def generate_stratified_plot_by_column(column_name, save_name):
+    unique_values = df[column_name].unique()
+    num_values = len(unique_values)
     
-    plt.figure(figsize=(12, 6))
-    df_grouped.unstack()['Total minimizer seeds'].plot(kind='bar', position=0, width=0.4, color=colors_minimizer)
-    df_grouped.unstack()['Total rymer seeds'].plot(kind='bar', position=1, width=0.4, color=colors_rymer)
-
-    # Add title, labels, and other plot properties
-    plt.title(f'Distribution of Minimizer and Rymer Seed Counts by Sketch Type (k={k})')
-    plt.xlabel('Number of Reads')
-    plt.ylabel('Seed Counts')
-    plt.xticks(rotation=45)
-    plt.legend(['Minimizer (Naive)', 'Minimizer (Unique)', 'Rymer (Naive)', 'Rymer (Unique)'])
-    plt.grid(axis='y')
-    plt.tight_layout()
-    plt.savefig(f'plots/single_plot_k_{k}.png')
-
-def generate_stratified_plot_by_k_save():
-    unique_kmer_sizes = df['k'].unique()
-    num_kmers = len(unique_kmer_sizes)
-    
-    fig, axes = plt.subplots(nrows=num_kmers, figsize=(14, 6 * num_kmers), sharex=True, sharey=True)
+    fig, axes = plt.subplots(nrows=num_values, figsize=(14, 6 * num_values), sharex=True, sharey=True)
 
     # Ensure axes is always a list for consistent indexing
-    if num_kmers == 1:
+    if num_values == 1:
         axes = [axes]
 
-    for i, k in enumerate(sorted(unique_kmer_sizes)):
+    for i, value in enumerate(sorted(unique_values)):
         ax = axes[i]
-        df_k = df[df['k'] == k]
+        df_filtered = df[df[column_name] == value]
         
-        df_grouped = df_k.groupby(['N', 'Minimizer sketch']).mean()[['Total minimizer seeds', 'Total rymer seeds']]
+        df_grouped = df_filtered.groupby(['N', 'Minimizer sketch']).mean()[['Total minimizer seeds', 'Total rymer seeds']]
         
-        # Plotting for each k-mer size
+        # Plotting for each unique value
         df_grouped.unstack()['Total minimizer seeds'].plot(kind='bar', position=0, width=0.4, ax=ax, color=colors_minimizer)
         df_grouped.unstack()['Total rymer seeds'].plot(kind='bar', position=1, width=0.4, ax=ax, color=colors_rymer)
 
         # Add title, labels, and other plot properties for each subplot
-        ax.set_title(f'Distribution of Minimizer and Rymer Seed Counts by Sketch Type (k={k})')
+        ax.set_title(f'Distribution of Minimizer and Rymer Seed Counts by Sketch Type ({column_name}={value})')
         ax.set_xlabel('Number of Reads')
         ax.set_ylabel('Seed Counts')
         ax.set_xticklabels(df_grouped.index.levels[0], rotation=45)
@@ -53,7 +36,7 @@ def generate_stratified_plot_by_k_save():
         ax.grid(axis='y')
     
     plt.tight_layout()
-    plt.savefig('plots/stratified_plot_by_k.png')
+    plt.savefig(f'plots/{save_name}.png')
 
 # Create a directory for plots if it doesn't exist
 if not os.path.exists('plots'):
@@ -64,8 +47,7 @@ df = pd.read_csv('results.tsv', sep='\t')
 
 # Call the functions to generate and save plots
 # Uncomment the following lines to generate the plots:
-#generate_single_plot_save(<specific k value>)
-generate_stratified_plot_by_k_save()
-
-# Replace <specific k value> with a k-mer size if you want to generate a single plot for that k-mer size.
+generate_stratified_plot_by_column('delta', 'stratified_plot_by_delta')
+generate_stratified_plot_by_column('W', 'stratified_plot_by_W')
+generate_stratified_plot_by_column('L', 'stratified_plot_by_read_length')
 
