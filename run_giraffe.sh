ba344935ad895468ad3cbdc64589a29a9eb43979
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-08-24 11:32:55 +0200

    Update

--- run_giraffe.sh
+++ run_giraffe.sh
@@ -4,7 +4,7 @@ hcfiles="/home/projects/MAAG/Magpie/Magpie/vgan_corrected/share/vgan/hcfiles"
                               -b fast -d $hcfiles/graph.dist -Z $hcfiles/graph.giraffe.gbz  > corrected.gam && echo -e "\n\n"
 
 
-./vg_uncorrected/bin/vg giraffe -f  test_data/human/D1j_damaged.fq -t 60 -m $hcfiles/graph.min -q $hcfiles/graph.ry \
+./vg_uncorrected/bin/vg giraffe -f  test_data/human/D1j_damaged.fq -t 60 -m $hcfiles/graph.min \
                               -b fast -d $hcfiles/graph.dist -Z $hcfiles/graph.giraffe.gbz  > uncorrected.gam && echo -e "\n\n"
 
 #./vgan_corrected/dep/vg/bin/vg giraffe -f  test_data/three_dhigh_100_tinier.fq.gz -t 60 -m ../euka_dir/euka_db.min -q ../euka_dir/euka_db.ry \
