fe834057898217835b5dca3d2a899d5db5ee184d
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-09-29 15:45:55 +0200

    Update

--- linear_experiment/human_mito/evaluate.py
+++ linear_experiment/human_mito/evaluate.py
@@ -9,14 +9,16 @@ def parse_stat_file(file_path):
     
     stats = {}
     for line in lines:
-        key, value = line.strip().split(":")
-        stats[key.strip()] = value.strip()
+        parts = line.strip().split(":")
+        if len(parts) != 2:
+            continue  # Skip lines that do not have a key-value pair separated by a colon
+        key, value = parts
+        stats[key.strip()] = int(value.strip())
     
     return stats
 
 def get_aligner_name_and_damage_type(filename):
-    # Modified regex pattern to capture aligner names with underscores
-    pattern = re.compile(r'.*_l[0-9]+_([^_]+)_s[^_]+_([\w]+)\.stat')
+    pattern = re.compile(r'.*_l[0-9]+_([^_]+(?:_anc)?)_s[^_]+_([\w]+)\.stat')
     match = pattern.match(filename)
     if match:
         damage_type, aligner_name = match.groups()
@@ -26,43 +28,43 @@ def get_aligner_name_and_damage_type(filename):
 
 def compute_proportion(directory, output_csv_path):
     files = [f for f in os.listdir(directory) if f.endswith('.stat')]
-    damage_type_aligner_proportion_correct_sum = defaultdict(lambda: defaultdict(float))
-    damage_type_aligner_proportion_incorrect_sum = defaultdict(lambda: defaultdict(float))
-    damage_type_aligner_file_count = defaultdict(lambda: defaultdict(int))
+    
+    data = []
     
     for file in files:
         file_path = os.path.join(directory, file)
         stats = parse_stat_file(file_path)
-        total = stats.get('Total')
-        if total is not None:
-            total = int(total)
-        else:
-            continue
-        correct_map = int(stats['correctmap'])
-        mapped = int(stats['mapped'])
+        
+        # Get statistics
+        total_reads = stats.get('Total reads', 0)
+        mapped_to_mt = stats.get('Mapped to MT', 0)
+        mapped_to_mt_correct_location = stats.get('Mapped to MT (Correct Location)', 0)
+        mapped_to_mt_correct_location_mq_30 = stats.get('Mapped to MT (Correct Location, MQ>30)', 0)
+        mapped_not_to_mt = stats.get('Mapped NOT to MT', 0)
+        mapped_not_to_mt_mq_30 = stats.get('Mapped NOT to MT (MQ>30)', 0)
+        
         aligner_name, damage_type = get_aligner_name_and_damage_type(file)
-        proportion_correct = correct_map / 1000
-        proportion_incorrect = 0 if mapped == 0 else (mapped - correct_map) / mapped
-        damage_type_aligner_proportion_correct_sum[damage_type][aligner_name] += proportion_correct
-        damage_type_aligner_proportion_incorrect_sum[damage_type][aligner_name] += proportion_incorrect
-        damage_type_aligner_file_count[damage_type][aligner_name] += 1
+        
+        # Collect data for this file
+        data.append([damage_type, aligner_name, total_reads, mapped_to_mt, mapped_to_mt_correct_location,
+                     mapped_to_mt_correct_location_mq_30, mapped_not_to_mt, mapped_not_to_mt_mq_30])
     
     # Save the results to a CSV file
     with open(output_csv_path, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Damage_Type', 'Aligner_Name', 'Avg_Proportion_Correct', 'Avg_Proportion_Incorrect'])
         
-        for damage_type in sorted(damage_type_aligner_proportion_correct_sum.keys()):
-            aligner_dict = damage_type_aligner_proportion_correct_sum[damage_type]
-            for aligner_name, proportion_correct_sum in aligner_dict.items():
-                average_proportion_correct = proportion_correct_sum / damage_type_aligner_file_count[damage_type][aligner_name]
-                average_proportion_incorrect = damage_type_aligner_proportion_incorrect_sum[damage_type][aligner_name] / damage_type_aligner_file_count[damage_type][aligner_name]
-                csvwriter.writerow([damage_type, aligner_name, average_proportion_correct, average_proportion_incorrect])
+        # Write header
+        csvwriter.writerow(['Damage_Type', 'Aligner_Name', 'Total_Reads', 'Mapped_to_MT', 'Mapped_to_MT_Correct_Location',
+                            'Mapped_to_MT_Correct_Location_MQ>30', 'Mapped_NOT_to_MT', 'Mapped_NOT_to_MT_MQ>30'])
+        
+        # Write data rows
+        for row in data:
+            csvwriter.writerow(row)
 
 # Define the directory path and output CSV path
 directory_path = '/home/projects/MAAG/Magpie/Magpie/linear_experiment/human_mito/alignments'
-output_csv_path = 'average_proportions.csv'
+output_csv_path = 'new_stats.csv'
 
-# Compute and save the proportions
+# Compute and save the new statistics
 compute_proportion(directory_path, output_csv_path)
 
