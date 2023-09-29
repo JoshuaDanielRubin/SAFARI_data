5290b480ff994ded21a3a5fff448ee92af2a060b
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-09-29 12:45:04 +0200

    Update

--- linear_experiment/human_mito/parseBamMito.py
+++ linear_experiment/human_mito/parseBamMito.py
@@ -3,46 +3,69 @@
 import pysam
 import sys
 
-pysam.set_verbosity(0)
-bamInputFile = pysam.Samfile(sys.argv[1], "rb")
-
-mapped = 0
-unmapped = 0
-correctlocationmapped = 0
-numt = 0
-total = 0
-
+# Function to check if two intervals intersect
 def intersects(left1, right1, left2, right2):
     return not (right1 < left2 or left1 > right2)
 
-for read in bamInputFile:
-    total += 1
+def main(bam_file_path):
+    # Suppress SAM-format errors
+    pysam.set_verbosity(0)
 
-    if read.is_unmapped:
-        unmapped += 1
+    # Open the BAM file for reading
+    bamInputFile = pysam.Samfile(bam_file_path, "rb")
+
+    # Initialize counters
+    total = 0
+    mapped_to_mt = 0
+    mapped_to_mt_correct_location = 0
+    mapped_to_mt_correct_location_MQ_gt_30 = 0
+    mapped_not_mt = 0
+    mapped_not_mt_MQ_gt_30 = 0
+
+    # Loop through each read in the BAM file
+    for read in bamInputFile:
+        total += 1  # Increment total read counter
+
+        # Only consider mapped reads
+        if not read.is_unmapped:
+            # Check if read is from mitochondrial genome based on query name
+            if read.query_name.startswith("generation"):
+                mapped_to_mt += 1
+                
+                # Check for correct location and orientation
+                fields = read.query_name.split(":")
+                intcs = intersects(
+                    int(fields[2]), int(fields[3]),
+                    read.reference_start - 50, read.reference_start + read.reference_length + 50
+                )
+                if intcs:
+                    mapped_to_mt_correct_location += 1
+                    
+                    # Check for MQ > 30
+                    if read.mapping_quality > 30:
+                        mapped_to_mt_correct_location_MQ_gt_30 += 1
+            else:
+                mapped_not_mt += 1
+                
+                # Check for MQ > 30
+                if read.mapping_quality > 30:
+                    mapped_not_mt_MQ_gt_30 += 1
+
+    # Close the BAM file
+    bamInputFile.close()
+
+    # Output statistics in table form
+    print(f"Total reads: {total}")
+    print(f"-----------------------------------------")
+    print(f"Mapped to MT: {mapped_to_mt}")
+    print(f"Mapped to MT (Correct Location): {mapped_to_mt_correct_location}")
+    print(f"Mapped to MT (Correct Location, MQ>30): {mapped_to_mt_correct_location_MQ_gt_30}")
+    print(f"Mapped NOT to MT: {mapped_not_mt}")
+    print(f"Mapped NOT to MT (MQ>30): {mapped_not_mt_MQ_gt_30}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <bam_file_path>")
     else:
-        mapped += 1  # Now counting all mapped reads, not just those with a specific query name prefix
-        
-        if read.query_name[0:10] == "generation":
-            fields = read.query_name.split(":")
-            intcs = intersects(
-                int(fields[2]), int(fields[3]),
-                read.reference_start-50, read.reference_start+read.reference_length+50
-            )
-            if intcs:
-                correctlocationmapped += 1
-        else:
-            numt += 1
-
-bamInputFile.close()
-
-print("Total:\t" + str(total))
-print("unmapped:\t" + str(unmapped))
-print("unmapped%:\t" + str(100.0 * (unmapped / total) if total != 0 else 0.0))
-print("mapped:\t" + str(mapped))
-print("mapped%:\t" + str(100.0 * (mapped / total) if total != 0 else 0.0))
-print("correctmap:\t" + str(correctlocationmapped))
-print("correctmap%:\t" + str(100.0 * (correctlocationmapped / mapped) if mapped != 0 else 0.0))
-print("numt:\t" + str(numt))
-print("numt%:\t" + str(100.0 * (numt / total) if total != 0 else 0.0))
+        main(sys.argv[1])
 
