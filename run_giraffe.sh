7b48095d29513ceb4992db0f9c72beb242f616e9
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-10-09 17:39:18 +0200

    Update

--- run_giraffe.sh
+++ run_giraffe.sh
@@ -1,6 +1,6 @@
 eukadir="/home/projects/MAAG/Magpie/euka_dir/"
 
-./vgan_corrected/dep/vg/bin/vg giraffe -f  /home/projects2/euka_environments/env_cave_dhigh.fq.gz -t 60 -m $eukadir/euka_db.min -q $eukadir/euka_db.ry \
+./vgan_corrected/dep/vg/bin/vg safari -f  /home/projects2/euka_environments/env_cave_dhigh.fq.gz -t 60 -m $eukadir/euka_db.min -q $eukadir/euka_db.ry \
                               -b fast -d $eukadir/euka_db.dist -Z $eukadir/euka_db.giraffe.gbz > corrected.gam && echo -e "\n\n"
 
 ./vg_uncorrected/bin/vg giraffe -f  /home/projects2/euka_environments/env_cave_dhigh.fq.gz -t 60 -m $eukadir/euka_db.min \
