fe8f46c71db4893468eefab21fac3c38aac82112
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-10-09 20:07:33 +0200

    Update

--- run_giraffe_filter.sh
+++
@@ -1,8 +0,0 @@
-eukadir="/home/projects/MAAG/Magpie/euka_dir/"
-
-./vgan_corrected/dep/vg/bin/vg safari -f  /home/projects2/euka_environments/env_cave_dhigh.fq.gz -t 60 -m $eukadir/euka_db.min -q $eukadir/euka_db.ry \
-                              -b fast -d $eukadir/euka_db.dist -Z $eukadir/euka_db.giraffe.gbz | ./vgan_corrected/dep/vg/bin/vg filter -q 10 -o 1 - > corrected_filtered.gam && echo -e "\n\n"
-
-./vg_uncorrected/bin/vg giraffe -f  /home/projects2/euka_environments/env_cave_dhigh.fq.gz -t 60 -m $eukadir/euka_db.min \
-                              -b fast -d $eukadir/euka_db.dist -Z $eukadir/euka_db.giraffe.gbz  | ./vg_uncorrected/bin/vg filter -q 10 -o 1 - > uncorrected_filtered.gam && echo -e "\n\n"
-
