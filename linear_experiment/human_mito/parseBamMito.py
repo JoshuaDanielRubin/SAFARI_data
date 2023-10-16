cb9653fd7a5ee250c781e3db07a5cbb29f31d737
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-10-16 11:21:57 +0200

    Update

--- linear_experiment/human_mito/parseBamMito.py
+++ linear_experiment/human_mito/parseBamMito.py
@@ -2,10 +2,6 @@
 import pysam
 import sys
 
-# Function to check if two intervals intersect
-def intersects(left1, right1, left2, right2):
-    return not (right1 < left2 or left1 > right2)
-
 def main(bam_file_path):
     # Suppress SAM-format errors
     pysam.set_verbosity(0)
@@ -13,65 +9,35 @@ def main(bam_file_path):
     # Open the BAM file for reading
     bamInputFile = pysam.Samfile(bam_file_path, "rb")
 
-    # Initialize counters
-    total = 0
-    mapped_to_mt = 0
-    mapped_to_mt_correct_location = 0
-    mapped_to_mt_correct_location_MQ_gt_30 = 0
-    not_mapped_to_mt = 0
-    not_mapped_to_mt_MQ_gt_30 = 0
-    unmapped_reads = 0  # Initialize counter for unmapped reads
+    # Initialize counters for TP, FP, TN, and FN
+    TP, FP, TN, FN = 0, 0, 0, 0
 
     # Loop through each read in the BAM file
     for read in bamInputFile:
-        total += 1  # Increment total read counter
+        # Determine if read came from MT
+        is_from_mt = read.query_name.startswith("generation")
 
-        # Check for unmapped reads
-        if read.is_unmapped:
-            unmapped_reads += 1
-        else:
-            # Check if read is from mitochondrial genome based on query name
-            if read.query_name.startswith("generation"):
-                mapped_to_mt += 1
-                
-                # Check for correct location and orientation
-                fields = read.query_name.split(":")
-                intcs = intersects(
-                    int(fields[2]), int(fields[3]),
-                    read.reference_start - 50, read.reference_start + read.reference_length + 50
-                )
-                if intcs:
-                    mapped_to_mt_correct_location += 1
-                    
-                    # Check for MQ > 30
-                    if read.mapping_quality > 30:
-                        mapped_to_mt_correct_location_MQ_gt_30 += 1
-            else:
-                not_mapped_to_mt += 1
-                
-                # Check for MQ > 30
-                if read.mapping_quality > 30:
-                    not_mapped_to_mt_MQ_gt_30 += 1
+        # Check if read is mapped to MT
+        is_mapped_to_mt = not read.is_unmapped and (read.reference_name == "generation_0" or read.reference_name == "H2a2a1")
+
+        # Compute TP, FP, TN, and FN
+        if is_from_mt and is_mapped_to_mt:
+            TP += 1
+        elif not is_from_mt and is_mapped_to_mt:
+            FP += 1
+        elif is_from_mt and not is_mapped_to_mt:
+            FN += 1
+        elif not is_from_mt and not is_mapped_to_mt:
+            TN += 1
 
     # Close the BAM file
     bamInputFile.close()
 
-    # Assertions
-    assert total == (mapped_to_mt + not_mapped_to_mt + unmapped_reads), "Mismatch in total mapped and unmapped reads!"
-    assert mapped_to_mt == (mapped_to_mt_correct_location + (mapped_to_mt - mapped_to_mt_correct_location)), "Mismatch in MT mapped reads!"
-    assert mapped_to_mt_correct_location_MQ_gt_30 <= mapped_to_mt_correct_location, "More reads with MQ>30 than reads in the correct location!"
-    assert not_mapped_to_mt_MQ_gt_30 <= not_mapped_to_mt, "More non-MT reads with MQ>30 than total non-MT reads!"
-    assert (mapped_to_mt + not_mapped_to_mt) == (total - unmapped_reads), "Mismatch in MT vs non-MT reads!"
-
-    # Output statistics in table form
-    print(f"Total reads: {total}")
-    print(f"-----------------------------------------")
-    print(f"Unmapped reads: {unmapped_reads}")
-    print(f"Mapped to MT: {mapped_to_mt}")
-    print(f"Mapped to MT (Correct Location): {mapped_to_mt_correct_location}")
-    print(f"Mapped to MT (Correct Location, MQ>30): {mapped_to_mt_correct_location_MQ_gt_30}")
-    print(f"NOT Mapped to MT: {not_mapped_to_mt}")
-    print(f"NOT Mapped to MT (MQ>30): {not_mapped_to_mt_MQ_gt_30}")
+    # Output the computed values
+    print(f"True Positives (TP): {TP}")
+    print(f"False Positives (FP): {FP}")
+    print(f"True Negatives (TN): {TN}")
+    print(f"False Negatives (FN): {FN}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
