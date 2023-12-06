bf913b97a1f3665df3b17aa33559942338cdb398
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-12-06 09:49:26 +0100

    Update

--- linear_experiment/human_mito/parseBamMito.py
+++ linear_experiment/human_mito/parseBamMito.py
@@ -1,59 +1,57 @@
 #!/usr/bin/python
+
 import pysam
-import sys
+import sys; 
 
-def main(bam_file_path):
-    # Suppress SAM-format errors
-    pysam.set_verbosity(0)
+pysam.set_verbosity(0)
+bamInputFile  = pysam.Samfile(sys.argv[1], "rb");
+#bamOutputFile = pysam.Samfile(sys.argv[2], "wb", template=bamInputFile);
 
-    # Open the BAM file for reading
-    bamInputFile = pysam.Samfile(bam_file_path, "rb")
+mapped=0;
+unmapped=0;
+correctlocationmapped=0;
+numt=0;
+total=0;
+def intersects(left1,right1,left2,right2):
+    return not (right1 < left2 or left1 > right2)
 
-    # Initialize counters for TP, FP, TN, FN and their counterparts with MQ > 30
-    TP, FP, TN, FN = 0, 0, 0, 0
-    TP_MQ30, FP_MQ30, TN_MQ30, FN_MQ30 = 0, 0, 0, 0
+for read in bamInputFile:
+    if(read.is_unmapped):
+        unmapped+=1;
+        continue;
 
-    # Loop through each read in the BAM file
-    for read in bamInputFile:
-        # Determine if read came from MT
-        is_from_mt = read.query_name.startswith("generation")
-
-        # Check if read is mapped to MT
-        is_mapped_to_mt = not read.is_unmapped and (read.reference_name == "generation_0" or read.reference_name == "H2a2a1")
-
-        # Compute TP, FP, TN, and FN
-        if is_from_mt and is_mapped_to_mt:
-            TP += 1
-            if read.mapping_quality > 30:
-                TP_MQ30 += 1
-        elif not is_from_mt and is_mapped_to_mt:
-            FP += 1
-            if read.mapping_quality > 30:
-                FP_MQ30 += 1
-        elif is_from_mt and not is_mapped_to_mt:
-            FN += 1
-            FN_MQ30 += 1  # Unmapped reads from MT 
-        elif not is_from_mt and not is_mapped_to_mt:
-            TN += 1
-            TN_MQ30 += 1  # Unmapped reads not from MT
-
-    # Close the BAM file
-    bamInputFile.close()
-
-    # Output the computed values
-    print(f"True Positives (TP): {TP}")
-    print(f"False Positives (FP): {FP}")
-    print(f"True Negatives (TN): {TN}")
-    print(f"False Negatives (FN): {FN}")
-    print("\nFor reads with MQ > 30:")
-    print(f"True Positives (TP_MQ30): {TP_MQ30}")
-    print(f"False Positives (FP_MQ30): {FP_MQ30}")
-    print(f"True Negatives (TN_MQ30): {TN_MQ30}")
-    print(f"False Negatives (FN_MQ30): {FN_MQ30}")
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <bam_file_path>")
+    
+    if(read.query_name[0:10] == "generation"):
+        mapped+=1;
+        fields=read.query_name.split(":");
+        #print(fields);
+        #print(read.reference_start)
+        #print(read.reference_start+read.reference_length);
+        intcs=intersects(int(fields[2]),int(fields[3]),read.reference_start-50,read.reference_start+read.reference_length+50);
+        #print(intcs);
+        if(intcs):
+            correctlocationmapped+=1;
+            
     else:
-        main(sys.argv[1])
+        numt+=1;
+    total+=1;
+    
+    #bamOutputFile.write(read);
+                
+bamInputFile.close();
+#bamOutputFile.close();
+
+
+print("Total:\t"+str(total));
+print("unmapped:\t"+str(unmapped));
+print("unmapped%:\t"+str(100*(unmapped/total)));
+print("mapped:\t"+str(mapped));
+print("mapped%:\t"+str(100*(mapped/total)));
+print("correctmap:\t"+str(correctlocationmapped));
+print("correctmap%:\t"+str(100*(correctlocationmapped/mapped)));
+print("numt:\t"+str(numt));
+print("numt%:\t"+str(100*(numt/total)));
+
+
+
 
